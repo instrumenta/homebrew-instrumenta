7c290b095a55c6057cfb61a643b2865ff0f15af0
goreleaserbot <goreleaser@carlosbecker.com>
2019-10-04 10:31:35 -0400

    Brew formula update for conftest version v0.14.0

--- Formula/conftest.rb
+++ Formula/conftest.rb
@@ -2,16 +2,16 @@
 class Conftest < Formula
   desc "Test your configuration using Open Policy Agent"
   homepage "https://github.com/instrumenta/conftest"
-  version "0.13.0"
+  version "0.14.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/instrumenta/conftest/releases/download/v0.13.0/conftest_0.13.0_Darwin_x86_64.tar.gz"
-    sha256 "125cbf3de25dae824de2781c1ab971ee13964659dbfa6ddb2477fdb89aecc11b"
+    url "https://github.com/instrumenta/conftest/releases/download/v0.14.0/conftest_0.14.0_Darwin_x86_64.tar.gz"
+    sha256 "16ea872ce2f8597f05cf5a4a7590e3aa0a125f4e70834adb0d40a5d1cf7184c7"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/instrumenta/conftest/releases/download/v0.13.0/conftest_0.13.0_Linux_x86_64.tar.gz"
-      sha256 "157c4ff3af9c0a49a268a25ea7597c54ef5021a7d48efc1fb7f4c779cbeb2900"
+      url "https://github.com/instrumenta/conftest/releases/download/v0.14.0/conftest_0.14.0_Linux_x86_64.tar.gz"
+      sha256 "23f2d799ea6bc3687863de962392940f7bb5768d507c7d43e30e468a308c68ea"
     end
   end
 
