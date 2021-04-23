9597e8018e2dcb19489ff552b3ff0850b9c30050
Charlie Getzen <charliegetzenlc@gmail.com>
2021-04-23 14:14:41 -0500

    Update to latest kubeval

--- Formula/kubeval.rb
+++ Formula/kubeval.rb
@@ -2,16 +2,16 @@
 class Kubeval < Formula
   desc "Validate your Kubernetes configurations"
   homepage "https://github.com/instrumenta/kubeval"
-  version "0.15.0"
+  version "0.16.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/instrumenta/kubeval/releases/download/0.15.0/kubeval-darwin-amd64.tar.gz"
-    sha256 "f445e9a7ff30645fb3850cdbdf9031095b14d5a9e524113dad6fc76d6e11d605"
+    url "https://github.com/instrumenta/kubeval/releases/download/v0.16.1/kubeval-darwin-amd64.tar.gz"
+    sha256 "c79a91f2e6638463881a8189e0628ebd583a5e2912e6f411897e3cea567125e7"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/instrumenta/kubeval/releases/download/0.15.0/kubeval-linux-amd64.tar.gz"
-      sha256 "70bff2642a2886c0d9ebea452ffb81f333a956e26bbe0826fd7c6797e343e5aa"
+      url "https://github.com/instrumenta/kubeval/releases/download/v0.16.1/kubeval-linux-amd64.tar.gz"
+      sha256 "2d6f9bda1423b93787fa05d9e8dfce2fc1190fefbcd9d0936b9635f3f78ba790"
     end
   end
 
